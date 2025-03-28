ac7ab5da9d0fa4992abe8d415ff01a05bec2a9a4
Kanishk <workforkanishk@gmail.com>
2025-03-28 15:01:01 +0530

    2nd commit, a simple search logic is added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Mcard from "../components/Mcard"
+import { useState } from "react"
 
 function Home(){
+    const [searchQuery, setSearchQuery]= useState("");
     const movies= [
         {id:1, title:"Zombieland",release_date:2009},
         {id:2, title:"Masaan",release_date:2015},
@@ -9,18 +11,23 @@ function Home(){
         {id:5, title:"Laapata Ladies",release_date:2023}
     ]
 
-    const handleSearch= ()=>{
-
+    const handleSearch= (e)=>{
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
     }
 
     return (
         <div className="Home">
             <form onSubmit={handleSearch} className="search-form">
-                <input type="text" placeholder="Search Movie Here..." className="search-input" />
+                <input type="text" placeholder="Search Movie Here..." className="search-input"
+                value={searchQuery}
+                onChange={(e)=>setSearchQuery(e.target.value)} />
             </form>
             <button type="submit" className="search-button">Search</button>
             <div className="movies-grid">
-                {movies.map((movie) => ( 
+                {movies.map((movie) => 
+                movie.title.toLowerCase().startsWith(searchQuery) && ( 
                     <Mcard movie={movie} key={movie.id} />
                     ))}
             </div>
@@ -29,3 +36,5 @@ function Home(){
 }
 
 export default Home
+// if you're dynamically rendering multiple things like movies.map , do add the .key property even if it isn't present in the component
+// so that react can identify this component compared to other ones that are being rendered
